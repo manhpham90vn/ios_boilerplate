e4eac84da28b22b5c9e6df0b1ccaf68204e28e7b
Manh Pham <manhpham90vn@icloud.com>
2021-02-08 13:42:17 +0700

    init mvc

---
+++ MVC/MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Manh Pham on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
