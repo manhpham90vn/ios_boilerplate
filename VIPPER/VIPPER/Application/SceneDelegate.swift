909fc93bd3b72aa9beddc46f5ce4e53d4036f023
Manh Pham <manhpham90vn@icloud.com>
2021-06-06 01:02:00 +0700

    fix init route in scenedelegate

--- VIPPER/VIPPER/Application/SceneDelegate.swift
+++ VIPPER/VIPPER/Application/SceneDelegate.swift
@@ -30,7 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        let vc = AuthManager.shared.isLogin ? MainRouter().viewController : LoginRouter().viewController
+        let vc = AuthManager.shared.isLogin ? AppRouter.main.viewController : AppRouter.login.viewController
         window?.rootViewController = UINavigationController(rootViewController: vc)
         window?.makeKeyAndVisible()
         
