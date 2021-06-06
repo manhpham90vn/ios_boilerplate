0944f11194c417ef12c8807fbd80162a57dfb490
Manh Pham <manhpham90vn@icloud.com>
2021-06-07 00:44:38 +0700

    add template

---
+++ VIPPER/VIPPER.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: BaseViewController {
+    
+    var presenter: ___VARIABLE_productName___PresenterInterface!
+
+    deinit {
+        print("\(type(of: self)) Deinit")
+        LeakDetector.instance.expectDeallocate(object: presenter as AnyObject)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___ViewInterface {
+    func showAlert(title: String, message: String) {
+
+    }
+
+    func showLoading(isLoading: Bool) {
+        
+    }
+}
