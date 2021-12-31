4d0b0d9e3491c05e0ee0b64510cc946aeb3ad5c5
Manh Pham <manhpham90vn@icloud.com>
2021-12-31 19:44:00 +0700

    handle github api error

---
+++ Sources/Common/Networking/AppCommonErrorInterface.swift
@@ -0,0 +1,38 @@
+//
+//  AppCommonErrorInterface.swift
+//  MyApp
+//
+//  Created by Manh Pham on 12/31/21.
+//
+
+import Foundation
+
+protocol AppCommonErrorInterface {
+    var errorTitle: String? { get }
+    var errorMessage: String? { get }
+    var buttonLeft: String? { get }
+    var buttonRight: String? { get }
+    var needHandleError: Bool { get }
+}
+
+extension AppCommonErrorInterface {
+    var errorTitle: String? {
+        "Đã xảy ra lỗi"
+    }
+    
+    var errorMessage: String? {
+        "Vui lòng thử lại"
+    }
+    
+    var buttonLeft: String? {
+        "Huỷ bỏ"
+    }
+    
+    var buttonRight: String? {
+        "Xác nhận"
+    }
+    
+    var needHandleError: Bool {
+        true
+    }
+}
