77fbfabb974313b7f1c5ebdadc442d2ca32701a9
Manh Pham <manhpham90vn@icloud.com>
2022-01-07 13:35:18 +0700

    refactor

--- Sources/Common/Extensions/PKHUD+Extensions.swift
+++ Sources/Common/Extensions/PKHUD+Extensions.swift
@@ -10,15 +10,12 @@ import PKHUD
 
 extension Reactive where Base: PKHUD {
 
-    // TODO: fix can not tap to alert whem device offline
     static var isAnimating: Binder<Bool> {
         return Binder(UIApplication.shared) {_, isVisible in
             if isVisible {
                 HUD.show(.progress)
-//                UIApplication.shared.beginIgnoringInteractionEvents()
             } else {
                 HUD.hide()
-//                UIApplication.shared.endIgnoringInteractionEvents()
             }
         }
     }
