56fcc5772971c07f5ffcbdac4f275288dfb0b352
Manh Pham <manhpham90vn@icloud.com>
2022-03-10 22:56:16 +0700

    update

--- scripts/installBrew.sh
+++ scripts/installBrew.sh
@@ -10,14 +10,6 @@ PACKAGES=(
     swiftlint
 )
 
-echo "Starting setup"
-
-if ! [ -e $BREW_CMD ]; then
-    echo "Installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    exit
-fi
-
 echo "Installing packages..."
 $BREW_CMD install ${PACKAGES[@]}
 
