e7f0e8f1b8477405e69589b7a452205aac921300
Manh Pham <manhpham90vn@icloud.com>
2022-08-21 09:36:55 +0700

    update connectivity service

--- Sources/Services/ConnectivityService.swift
+++ Sources/Services/ConnectivityService.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import SystemConfiguration
+import Alamofire
 
 protocol ConnectivityService {
     var isNetworkConnection: Bool { get }
@@ -14,20 +14,6 @@ protocol ConnectivityService {
 
 final class ConnectivityServiceImpl: ConnectivityService {
     var isNetworkConnection: Bool {
-        var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
-        zeroAddress.sin_family = sa_family_t(AF_INET)
-        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { zeroSockAddress in
-                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
-            }
-        }
-        var flags = SCNetworkReachabilityFlags()
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
-            return false
-        }
-        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
-        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-        return (isReachable && !needsConnection)
+        return NetworkReachabilityManager()?.isReachable ?? false
     }
 }
