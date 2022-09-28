48212d6827a53feb3fb0ebbc4dac5076f84406a9
Manh Pham <manhpham90vn@icloud.com>
2022-09-28 17:28:44 +0700

    fix script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,7 +1,34 @@
 #!/bin/sh
 
-bash scripts/installDependencies.sh
+if ! rbenv -v &> /dev/null
+then
+    brew install rbenv
+fi
+
+if ! xcodegen --version &> /dev/null
+then
+    brew install xcodegen
+fi
+
+if ! swiftgen --version &> /dev/null
+then
+    brew install swiftgen
+fi
+
+if ! swiftlint --version &> /dev/null
+then
+    brew install swiftlint
+fi
+
+if ! carthage version &> /dev/null
+then
+    brew install carthage
+fi
+
 mkdir -p Sources/Common/Resources/Generated/SwiftGen
+
 swiftgen
+
 xcodegen generate --spec project.yml
+
 pod install --repo-update
