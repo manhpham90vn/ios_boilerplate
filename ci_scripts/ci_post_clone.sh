72110240df89402a0666d7d8068297d6879218ea
Manh Pham (OFA iOS) <manhpham90vn@icloud.com>
2022-11-22 11:51:04 +0700

    fix xcodecloud (#67)
    * fix xcodecloud
    
    * fix
    
    * fix

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,19 +3,14 @@
 cd $CI_WORKSPACE
 HOMEBREW_NO_AUTO_UPDATE=1
 
-if ! xcodegen --version &> /dev/null
+if ! rbenv -v &> /dev/null
 then
-    brew install xcodegen
+    brew install rbenv
 fi
 
-if ! swiftgen --version &> /dev/null
+if ! mint version &> /dev/null
 then
-    brew install swiftgen
-fi
-
-if ! swiftlint --version &> /dev/null
-then
-    brew install swiftlint
+    brew install mint
 fi
 
 if ! pod --version &> /dev/null
@@ -23,10 +18,12 @@ then
     brew install cocoapods
 fi
 
+mint bootstrap --link
+
+rm -rf Sources/Common/Resources/Generated/*
 mkdir -p Sources/Common/Resources/Generated/SwiftGen
+mint run SwiftGen/SwiftGen@6.5.1 swiftgen
 
-swiftgen config run --config swiftgen.yml
-
-xcodegen generate --spec project.yml
+mint run yonaskolb/xcodegen@2.32.0 xcodegen generate --spec project.yml
 
 pod install
