6e6b62d9ada66546ca24532324a4a661ce6efce0
Manh Pham (OFA iOS) <manhpham90vn@icloud.com>
2022-12-05 23:08:31 +0700

    sample unit test (#76)

---
+++ SourcesTests/AppInjector.swift
@@ -0,0 +1,17 @@
+//
+//  AppInjector.swift
+//  MyApp
+//
+//  Created by Manh Pham on 05/12/2022.
+//
+
+import Foundation
+import MPInjector
+@testable import My_App_Debug
+
+extension MPInjector: RegisteringMock {
+    public func registerServiceMock() {
+        registerMock { UserRepositoryMock() as UserRepositoryInterface }
+        registerMock { LoginRouterMock() as LoginRouterInterface }
+    }
+}
--- SourcesTests/MyiOSAppTests.swift
+++ SourcesTests/MyiOSAppTests.swift
@@ -6,10 +6,24 @@
 //
 
 import XCTest
+import MPInjector
 @testable import My_App_Debug
 
 final class MyiOSAppTests: XCTestCase {
-    func testExample() throws {
-        XCTAssertEqual(1, Int("1"))
+    
+    @Inject var local: LocalStorageRepository
+    
+    func testLoginUseCase() {
+        let login = LoginUseCase()
+        login.execute(params: .init(email: "test", password: "test"))
+        XCTAssertEqual(local.getAccessToken(), "testToken")
+    }
+    
+    func testLoginPresenter() {
+        let pr = LoginPresenter()
+        pr.login.accept("test")
+        pr.password.accept("test")
+        pr.didTapLoginButton()
+        XCTAssertEqual((pr.router as? LoginRouterMock)?.didCallNavigationToHomeScreen, true)
     }
 }
