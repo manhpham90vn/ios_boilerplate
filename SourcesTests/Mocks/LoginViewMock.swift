398ef42a7e69e161e757eab89f36d2f896fc9bac
Manh Pham <manhpham90vn@icloud.com>
2022-12-06 10:26:58 +0700

    update unit test

---
+++ SourcesTests/Mocks/LoginViewMock.swift
@@ -0,0 +1,11 @@
+//
+//  LoginViewMock.swift
+//  MyAppTests
+//
+//  Created by Manh Pham on 06/12/2022.
+//
+
+import Foundation
+@testable import My_App_Debug
+
+class LoginViewMock: LoginViewInterface {}
