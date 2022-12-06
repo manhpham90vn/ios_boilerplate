e8cb3137e32239e16d5dc60929366003c9b3c5c6
Manh Pham (OFA iOS) <manhpham90vn@icloud.com>
2022-12-06 11:44:23 +0700

    update unit test (#77)

--- SourcesTests/Mocks/UserRepositoryMock.swift
+++ SourcesTests/Mocks/UserRepositoryMock.swift
@@ -7,11 +7,16 @@
 
 import Foundation
 import RxSwift
+import Networking
 @testable import My_App_Debug
 
 class UserRepositoryMock: UserRepositoryInterface {
     func login(email: String, password: String) -> Single<LoginResponse> {
-        return .just(.init(token: "testToken", refreshToken: "testRefreshToken"))
+        if email == "test" {
+            return .just(.init(token: "testToken", refreshToken: "testRefreshToken"))
+        } else {
+            return .error(AppError.noInternetConnection)
+        }
     }
 
     func userInfo() -> Single<UserResponse> {
