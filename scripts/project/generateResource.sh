e9f0e30f3949c5b5d008cd1ca8b01b0e72c9190f
Manh Pham <manhpham90vn@icloud.com>
2022-12-25 23:13:52 +0700

    fix

--- scripts/project/generateResource.sh
+++ scripts/project/generateResource.sh
@@ -3,6 +3,8 @@
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../common/version.sh
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../mint/mint-run.sh
 
+cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../
+
 rm -rf Sources/Common/Resources/Generated/*
 mkdir -p Sources/Common/Resources/Generated/SwiftGen
 
