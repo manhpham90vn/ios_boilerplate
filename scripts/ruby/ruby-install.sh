09a4289d15b0f1b97553201afe36905d369efd2e
Manh Pham <manhpham90vn@icloud.com>
2022-12-25 21:57:10 +0700

    update

--- scripts/ruby/ruby-install.sh
+++ scripts/ruby/ruby-install.sh
@@ -1,7 +1,45 @@
 #!/bin/sh
 
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/ruby-setup.sh
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/ruby-cmd.sh
+
+if ! $RBENV_CMD --version &> /dev/null
+then
+    sh $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../brew/brew-install-package.sh $RBENV_CMD
+fi
+
+PROJECT_RUBY_VERSION_PATH=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../.ruby-version
+PROJECT_RUBY_VERSION=`cat $PROJECT_RUBY_VERSION_PATH`
+
+$RBENV_CMD install -s $PROJECT_RUBY_VERSION
+$RBENV_CMD global $PROJECT_RUBY_VERSION
+
+case $SHELL in
+*/bash)
+    SHELL_RUN_CMD_PATH="${HOME}/.bashrc"
+    break
+;;
+*/zsh)
+  	SHELL_RUN_CMD_PATH="${HOME}/.zshrc"
+  	break
+;;
+*/fish)
+  	SHELL_RUN_CMD_PATH="${HOME}/.config/fish/config.fish"
+  	break
+;;
+esac
+
+RBENV_CONFIG_EXPORT_PATH="export PATH=\"\$HOME/.rbenv/bin:\$PATH\""
+RBENV_CONFIG_INIT="eval \"\$(rbenv init -)\""
+
+if ! ( grep -q "${RBENV_CONFIG_EXPORT_PATH}" "${SHELL_RUN_CMD_PATH}" ); then
+    echo $RBENV_CONFIG_EXPORT_PATH >> $SHELL_RUN_CMD_PATH
+fi
+if ! ( grep -q "${RBENV_CONFIG_INIT}" "${SHELL_RUN_CMD_PATH}" ); then
+    echo $RBENV_CONFIG_INIT >> $SHELL_RUN_CMD_PATH
+fi
+
+source $SHELL_RUN_CMD_PATH
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/ruby-setup.sh
 
 if ! $BUNDLER_CMD --version &> /dev/null
 then
