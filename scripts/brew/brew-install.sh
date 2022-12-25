46bfaac8b16e43159064b7ea086d643f0bb55c8e
Manh Pham <manhpham90vn@icloud.com>
2022-12-26 00:08:43 +0700

    ok

--- scripts/brew/brew-install.sh
+++ scripts/brew/brew-install.sh
@@ -2,6 +2,8 @@
 
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/brew-cmd.sh
 
+cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../
+
 if ! $BREW_CMD --version &> /dev/null
 then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
