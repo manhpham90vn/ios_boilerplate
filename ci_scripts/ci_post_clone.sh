63a0abab04ce148d4b88a08b819e79451999b7fa
Manh Pham <manhpham90vn@icloud.com>
2022-12-25 22:01:43 +0700

    update xcodecloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,23 +2,9 @@
 
 cd $CI_WORKSPACE
 
-if ! rbenv --version &> /dev/null
-then
-    brew install rbenv
-    rbenv install 2.6.9
-    rbenv global 2.6.9
-fi
-
-echo 'eval "$(rbenv init -)"' >> ~/.zshrc
-source ~/.zshrc
-
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/ruby/install.sh
-
-bundle config path vendor/bundle
-bundle install --without=documentation --jobs 4 --retry 3
-
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/mint/install.sh
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/mint/generateResource.sh
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/mint/generateProject.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/ruby/ruby-install.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/mint/mint-run.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/project/generateResource.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/project/generateProject.sh
 
 bundle exec pod install
