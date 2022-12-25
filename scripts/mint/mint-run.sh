5182de2317aa3c2d92e30908d49e4679fc161524
Manh Pham <manhpham90vn@icloud.com>
2022-12-26 00:08:43 +0700

    ok

--- scripts/mint/mint-run.sh
+++ scripts/mint/mint-run.sh
@@ -2,7 +2,9 @@
 
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/mint-install.sh
 
-if ! [ `$(ls -A $MINT_LINK_PATH)` ]
+cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../
+
+if ! [ "$(ls -A $MINT_LINK_PATH)" ]
 then
     $MINT_CMD bootstrap -m $MINT_FILE --link
 fi
