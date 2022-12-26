f643c8d6797dc8fdf0a30d426e678667e5c5ebe1
Manh Pham <manhpham90vn@icloud.com>
2022-12-26 10:39:57 +0700

    update

--- scripts/pod/pod-run.sh
+++ scripts/pod/pod-run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/pod-run.sh
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../ruby/ruby-install/pod-run.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/pod-cmd.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../ruby/ruby-install.sh
+
+cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../
 
 if ! $BUNDLER_CMD exec pod --version &> /dev/null
 then
