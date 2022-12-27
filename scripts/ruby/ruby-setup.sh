7dc650eb0593b79eab565e4a3ebe2bcadf7d5a6c
Manh Pham <manhpham90vn@icloud.com>
2022-12-27 15:49:34 +0700

    fix

--- scripts/ruby/ruby-setup.sh
+++ scripts/ruby/ruby-setup.sh
@@ -5,7 +5,7 @@ source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/ruby-cmd.sh
 cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../
 
 PROJECT_BUNDLER_VERSION=$(cat < Gemfile.lock | tail -1 | tr -d " ")
-CURRENT_BUNDLER_VERSION=$($BUNDLER_CMD --version > /dev/null 2>&1 | grep -o -E "Bundler version [0-9]+\\.[0-9]+\\.[0-9]+" | cut -d' ' -f3)
+CURRENT_BUNDLER_VERSION=$($BUNDLER_CMD --version | grep -o -E "Bundler version [0-9]+\\.[0-9]+\\.[0-9]+" | cut -d' ' -f3)
 BUNDLER_PATH=vendor/bundle
 PROJECT_RUBY_VERSION=$(cat $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../.ruby-version)
 CURRENT_RUBY_VERSION=$($RUBY_CMD --version | grep -o -E "ruby [0-9]+\\.[0-9]+\\.[0-9]+" | cut -d' ' -f2)
