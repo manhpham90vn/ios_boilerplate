c98591c1b6559f6232e9877765e8efd1c11b23fb
Manh Pham <manhpham90vn@icloud.com>
2022-12-30 09:44:10 +0700

    update

--- scripts/circleci/ci-generate.sh
+++ scripts/circleci/ci-generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../project/generate-resource.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../project/generate-swiftgen.sh
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../project/generate-project.sh
