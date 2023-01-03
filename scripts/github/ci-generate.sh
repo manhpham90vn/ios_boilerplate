afd977f121135fa97f3033f3d4a0703ff9ea4d74
Manh Pham (OFA iOS) <manhpham90vn@icloud.com>
2023-01-03 22:26:12 +0700

    Update ci-generate.sh

--- scripts/github/ci-generate.sh
+++ scripts/github/ci-generate.sh
@@ -3,6 +3,6 @@
 HOMEBREW_NO_AUTO_UPDATE=1
 
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../project/generate-swiftgen.sh
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../project/generate-project.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../xcodegen/xcodegen-run.sh
 
 bundler exec pod install
