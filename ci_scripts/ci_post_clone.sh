64233eb86a1ba683d055e6b94dfa83d31bd5e8d5
Manh Pham (OFA iOS) <manhpham90vn@icloud.com>
2023-01-03 22:51:04 +0700

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,5 +7,6 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/ruby/ruby-install.sh
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/mint/mint-run.sh
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/project/generate-swiftgen.sh
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/common/init.sh
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/xcodegen/xcodegen-run.sh
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../scripts/pod/pod-run.sh
