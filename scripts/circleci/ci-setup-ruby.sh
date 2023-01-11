ba579d0a27eadd5aaf79c80e1184997bbab487e0
Manh Pham <manhpham90vn@icloud.com>
2023-01-11 16:31:05 +0700

    f

--- scripts/circleci/ci-setup-ruby.sh
+++ scripts/circleci/ci-setup-ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo 'export GEM_CMD=gem' >> "$BASH_ENV"
-echo 'BUNDLER_CMD=bundler' >> "$BASH_ENV"
+echo 'export BUNDLER_CMD=bundler' >> "$BASH_ENV"
 
 source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../ruby/ruby-install.sh
