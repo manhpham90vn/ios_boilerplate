1b996dc1eef76632afd9954b7a596f0baf765a53
Manh Pham <manhpham90vn@icloud.com>
2023-01-16 00:05:30 +0700

    fix usecase

--- Sources/Common/Base/SingleUseCase.swift
+++ Sources/Common/Base/SingleUseCase.swift
@@ -12,70 +12,58 @@ import RxCocoa
 import MPInjector
 
 class SingleUseCase<P, R>: UseCase {
-
+    
     var cacheParams: P?
-
-    let trigger = PublishSubject<Void>()
+    
     private let bag = DisposeBag()
-
+    
     @Inject var connectivityService: ConnectivityService
-
+    
     private let _processing = BehaviorRelay(value: false)
     var processing: Driver<Bool> {
         _processing
             .asDriver()
     }
 
-    private let _succeeded = PublishSubject<R>()
+    private let _succeeded = PublishRelay<R>()
     var succeeded: Driver<R> {
         _succeeded
             .asDriverOnErrorJustComplete()
     }
 
-    private let _failed = PublishSubject<Error>()
+    private let _failed = PublishRelay<Error>()
     var failed: Driver<Error> {
         _failed
             .asDriverOnErrorJustComplete()
     }
-    
-    init() {
-        trigger
-            .map { [weak self] _ in
-                guard let self = self else { return false }
-                if self._processing.value {
-                    self._failed.onNext(AppError.actionAlreadyPerforming)
-                    return false
-                }
-                if !self.connectivityService.isNetworkConnection {
-                    self._failed.onNext(AppError.noInternetConnection)
-                    return false
-                }
-                self._processing.accept(true)
-                return true
-            }
-            .filter { $0 }
-            .flatMap { [weak self] _ -> Single<R> in
-                guard let self = self, let cacheParams = self.cacheParams else { return .never() }
-                return self.buildUseCase(params: cacheParams)
-            }
-            .asSingle()
-            .subscribe(onSuccess: { [weak self] result in
-                self?._succeeded.onNext(result)
-                self?._processing.accept(false)
-            }, onFailure: { [weak self] error in
-                self?._failed.onNext(error)
-                self?._processing.accept(false)
-            })
-            .disposed(by: bag)
-    }
-    
-    // do not call this function directly
+
     func buildUseCase(params: P) -> Single<R> { // swiftlint:disable:this unavailable_function
         fatalError("this is abstract")
     }
     
-    final func execute(params: P) {
+    func execute(params: P) {
         cacheParams = params
-        trigger.onNext(())
+        performedIfNeeded(params: params)
+    }
+    
+    func performedIfNeeded(params: P) {
+        if _processing.value == true {
+            _failed.accept(AppError.actionAlreadyPerforming)
+            return
+        }
+        if !connectivityService.isNetworkConnection {
+            _failed.accept(AppError.noInternetConnection)
+            return
+        }
+        _processing.accept(true)
+        buildUseCase(params: params)
+            .subscribe(onSuccess: { [weak self] result in
+                self?._succeeded.accept(result)
+                self?._processing.accept(false)
+            }, onFailure: { [weak self] error in
+                self?._failed.accept(error)
+                self?._processing.accept(false)
+            })
+            .disposed(by: bag)
     }
 }
