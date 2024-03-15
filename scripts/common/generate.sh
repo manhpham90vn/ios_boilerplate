b259a4e4382583932ebf90b9b5d990bf021b1e53
Manh Pham <manhpham90vn@icloud.com>
2024-03-16 00:36:29 +0700

    sync

--- scripts/common/generate.sh
+++ scripts/common/generate.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
+# generate swiftgen
+rm -rf Sources/Resources/Generated/*
+mkdir -p Sources/Resources/Generated/SwiftGen
+swiftgen
+
 # generate project
 xcodegen generate --spec project.yml
 
 # install pod
 pod install
-
-# generate swiftgen
-rm -rf Sources/Resources/Generated/*
-mkdir -p Sources/Resources/Generated/SwiftGen
-swiftgen
 
 # generate mock
 mockolo -s Sources -d SourcesTests/Mocks.swift --enable-args-history --mock-all --use-mock-observable --exclude-imports AppKit --testable-imports MyProduct
